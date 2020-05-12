d5a70002e5492fa0ef3a314b8b660b84f0108e04
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-05-12 21:38:25 +0900

    refactor

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -1,53 +1,136 @@
-use iou::SubmissionQueueEvent;
-use libc::{sockaddr, socklen_t};
-use std::os::unix::prelude::*;
+use std::{
+    io,
+    net::{TcpListener, TcpStream},
+    os::unix::prelude::*,
+    ptr,
+};
 
-pub trait SubmissionQueueEventExt {
-    unsafe fn prep_accept(
-        &mut self,
-        sock: RawFd,
-        addr: *mut sockaddr,
-        addrlen: *mut socklen_t,
-        flags: i32,
-    );
-    unsafe fn prep_readv(&mut self, fd: RawFd, bufs: &mut [libc::iovec], flags: libc::off_t);
-    unsafe fn prep_writev(&mut self, fd: RawFd, bufs: &[libc::iovec], flags: libc::off_t);
+pub enum EventType {
+    Accept,
+    Read,
+    Write,
 }
 
-impl SubmissionQueueEventExt for SubmissionQueueEvent<'_> {
-    unsafe fn prep_accept(
-        &mut self,
-        sock: RawFd,
-        addr: *mut sockaddr,
-        addrlen: *mut socklen_t,
-        flags: i32,
-    ) {
-        uring_sys::io_uring_prep_accept(
-            self.raw_mut(), //
-            sock,
-            addr,
-            addrlen,
-            flags,
-        );
+#[allow(dead_code)]
+pub struct Event {
+    pub event_type: EventType,
+    pub client_socket: Option<TcpStream>,
+    pub buf: Vec<u8>,
+    iovecs: Option<Box<[libc::iovec; 1]>>,
+}
+
+pub struct Uring(iou::IoUring);
+
+impl Uring {
+    pub fn new(entries: u32) -> io::Result<Self> {
+        iou::IoUring::new(entries).map(Self)
     }
 
-    unsafe fn prep_readv(&mut self, fd: RawFd, iovecs: &mut [libc::iovec], offset: libc::off_t) {
-        uring_sys::io_uring_prep_readv(
-            self.raw_mut(),
-            fd,
-            iovecs.as_mut_ptr(),
-            iovecs.len() as libc::c_uint,
-            offset,
-        )
+    pub fn wait_cqe(&mut self) -> io::Result<(Box<Event>, io::Result<usize>)> {
+        let cqe = self.0.wait_for_cqe()?;
+
+        let user_data = unsafe {
+            let ptr = cqe.user_data() as *mut Event;
+            Box::from_raw(ptr)
+        };
+
+        let res = cqe.result();
+
+        Ok((user_data, res))
     }
 
-    unsafe fn prep_writev(&mut self, fd: RawFd, iovecs: &[libc::iovec], offset: libc::off_t) {
-        uring_sys::io_uring_prep_writev(
-            self.raw_mut(),
-            fd,
-            iovecs.as_ptr(),
-            iovecs.len() as libc::c_uint,
-            offset,
-        )
+    pub fn next_sqe(&mut self) -> io::Result<SubmissionEvent<'_>> {
+        self.0.next_sqe().map(SubmissionEvent).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "io_uring submission queue is empty")
+        })
+    }
+
+    pub fn submit_sqes(&mut self) -> io::Result<()> {
+        self.0.submit_sqes()?;
+        Ok(())
+    }
+}
+
+pub struct SubmissionEvent<'a>(iou::SubmissionQueueEvent<'a>);
+
+impl SubmissionEvent<'_> {
+    pub fn accept(mut self, listener: &TcpListener) -> io::Result<()> {
+        let sqe = &mut self.0;
+
+        unsafe {
+            uring_sys::io_uring_prep_accept(
+                sqe.raw_mut(), //
+                listener.as_raw_fd(),
+                ptr::null_mut(),
+                ptr::null_mut(),
+                0,
+            );
+        }
+
+        let user_data = Box::new(Event {
+            event_type: EventType::Accept,
+            client_socket: None,
+            buf: Vec::new(),
+            iovecs: None,
+        });
+        sqe.set_user_data(Box::into_raw(user_data) as _);
+
+        Ok(())
+    }
+
+    pub fn read(mut self, client_socket: TcpStream, mut buf: Vec<u8>) -> anyhow::Result<()> {
+        let sqe = &mut self.0;
+
+        let mut iovecs = Box::new([libc::iovec {
+            iov_base: buf.as_mut_ptr().cast(),
+            iov_len: buf.len(),
+        }]);
+        unsafe {
+            uring_sys::io_uring_prep_readv(
+                sqe.raw_mut(),
+                client_socket.as_raw_fd(),
+                iovecs.as_mut_ptr(),
+                iovecs.len() as libc::c_uint,
+                0,
+            );
+        }
+
+        let user_data = Box::new(Event {
+            event_type: EventType::Read,
+            client_socket: Some(client_socket),
+            buf,
+            iovecs: Some(iovecs),
+        });
+        sqe.set_user_data(Box::into_raw(user_data) as _);
+
+        Ok(())
+    }
+
+    pub fn write(mut self, client_socket: TcpStream, buf: Vec<u8>) -> io::Result<()> {
+        let sqe = &mut self.0;
+
+        let iovecs = Box::new([libc::iovec {
+            iov_base: buf.as_ptr() as *mut _,
+            iov_len: buf.len(),
+        }]);
+        unsafe {
+            uring_sys::io_uring_prep_writev(
+                sqe.raw_mut(),
+                client_socket.as_raw_fd(),
+                iovecs.as_ptr(),
+                iovecs.len() as libc::c_uint,
+                0,
+            );
+        }
+
+        let user_data = Box::new(Event {
+            event_type: EventType::Write,
+            client_socket: Some(client_socket),
+            buf,
+            iovecs: Some(iovecs),
+        });
+        sqe.set_user_data(Box::into_raw(user_data) as _);
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,75 @@
 mod io_uring;
-mod server;
 
-use crate::server::Server;
+use crate::io_uring::{EventType, Uring};
+use anyhow::Context as _;
+use std::{
+    net::{TcpListener, TcpStream},
+    os::unix::prelude::*,
+};
+
+const QUEUE_DEPTH: u32 = 256;
+const READ_SIZE: usize = 8096;
 
 fn main() -> anyhow::Result<()> {
-    let addr = "127.0.0.1:8000";
-    println!("Listening on {}", addr);
-    let mut server = Server::bind(addr)?;
-    server.run_loop()?;
-    Ok(())
+    let listener = TcpListener::bind("127.0.0.1:8000") //
+        .context("failed to open listener socket")?;
+
+    if let Ok(local_addr) = listener.local_addr() {
+        println!("Listening on {}", local_addr);
+    }
+
+    let mut uring = Uring::new(QUEUE_DEPTH) //
+        .context("failed to init io_uring")?;
+    uring.next_sqe()?.accept(&listener)?;
+    uring.submit_sqes()?;
+
+    loop {
+        let (user_data, res) = uring.wait_cqe()?;
+        let n = res.context("async request failed")?;
+
+        match user_data.event_type {
+            EventType::Accept => {
+                uring.next_sqe()?.accept(&listener)?;
+
+                let client_socket = unsafe { TcpStream::from_raw_fd(n as _) };
+                let buf = vec![0u8; READ_SIZE];
+                uring.next_sqe()?.read(client_socket, buf)?;
+            }
+
+            EventType::Read if n == 0 => eprintln!("warning: empty request"),
+            EventType::Read => {
+                let buf = &user_data.buf[..n];
+
+                let mut headers = [httparse::EMPTY_HEADER; 16];
+                let mut req = httparse::Request::new(&mut headers);
+                let status = req
+                    .parse(buf) //
+                    .context("failed to parse http request")?;
+
+                let _amt = match status {
+                    httparse::Status::Complete(amt) => amt,
+                    httparse::Status::Partial => {
+                        anyhow::bail!("unimplemented: continue read request")
+                    }
+                };
+
+                // TODO: handle HTTP methods
+                uring.next_sqe()?.write(
+                    user_data.client_socket.unwrap(),
+                    "\
+                            HTTP/1.1 404 Not Found\r\n\
+                            Server: io-uring-http-server\r\n\
+                            Content-Length: 0\r\n\
+                            Date: Tue, 12 May 2020 09:44:32 GMT\r\n\
+                            \r\n\
+                        "
+                    .into(),
+                )?;
+            }
+
+            EventType::Write => { /* do nothing. */ }
+        }
+
+        uring.submit_sqes()?;
+    }
 }
