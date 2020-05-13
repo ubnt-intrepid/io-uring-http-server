678398cfd96c8c6af2eb7b0f1264745cc5a85d4b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-05-13 14:36:57 +0900

    use latest iou

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -2,7 +2,6 @@ use std::{
     io,
     net::{TcpListener, TcpStream},
     os::unix::prelude::*,
-    ptr,
 };
 
 pub enum EventType {
@@ -16,7 +15,6 @@ pub struct Event {
     pub event_type: EventType,
     pub client_socket: Option<TcpStream>,
     pub buf: Vec<u8>,
-    iovecs: Option<Box<[libc::iovec; 1]>>,
 }
 
 pub struct Uring(iou::IoUring);
@@ -58,20 +56,13 @@ impl SubmissionEvent<'_> {
         let sqe = &mut self.0;
 
         unsafe {
-            uring_sys::io_uring_prep_accept(
-                sqe.raw_mut(), //
-                listener.as_raw_fd(),
-                ptr::null_mut(),
-                ptr::null_mut(),
-                0,
-            );
+            sqe.prep_accept(listener.as_raw_fd(), None, iou::SockFlag::empty());
         }
 
         let user_data = Box::new(Event {
             event_type: EventType::Accept,
             client_socket: None,
             buf: Vec::new(),
-            iovecs: None,
         });
         sqe.set_user_data(Box::into_raw(user_data) as _);
 
@@ -81,25 +72,14 @@ impl SubmissionEvent<'_> {
     pub fn read(mut self, client_socket: TcpStream, mut buf: Vec<u8>) -> anyhow::Result<()> {
         let sqe = &mut self.0;
 
-        let mut iovecs = Box::new([libc::iovec {
-            iov_base: buf.as_mut_ptr().cast(),
-            iov_len: buf.len(),
-        }]);
         unsafe {
-            uring_sys::io_uring_prep_readv(
-                sqe.raw_mut(),
-                client_socket.as_raw_fd(),
-                iovecs.as_mut_ptr(),
-                iovecs.len() as libc::c_uint,
-                0,
-            );
+            sqe.prep_read(client_socket.as_raw_fd(), &mut buf[..], 0);
         }
 
         let user_data = Box::new(Event {
             event_type: EventType::Read,
             client_socket: Some(client_socket),
             buf,
-            iovecs: Some(iovecs),
         });
         sqe.set_user_data(Box::into_raw(user_data) as _);
 
@@ -109,25 +89,14 @@ impl SubmissionEvent<'_> {
     pub fn write(mut self, client_socket: TcpStream, buf: Vec<u8>) -> io::Result<()> {
         let sqe = &mut self.0;
 
-        let iovecs = Box::new([libc::iovec {
-            iov_base: buf.as_ptr() as *mut _,
-            iov_len: buf.len(),
-        }]);
         unsafe {
-            uring_sys::io_uring_prep_writev(
-                sqe.raw_mut(),
-                client_socket.as_raw_fd(),
-                iovecs.as_ptr(),
-                iovecs.len() as libc::c_uint,
-                0,
-            );
+            sqe.prep_write(client_socket.as_raw_fd(), &buf[..], 0);
         }
 
         let user_data = Box::new(Event {
             event_type: EventType::Write,
             client_socket: Some(client_socket),
             buf,
-            iovecs: Some(iovecs),
         });
         sqe.set_user_data(Box::into_raw(user_data) as _);
 
