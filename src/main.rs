6b8f3b5191692778a4f56a69cd9cdf20da774e67
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-05-13 17:24:56 +0900

    handle GET request

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,20 @@
 mod io_uring;
 mod request;
 
-use crate::io_uring::{EventType, Uring};
+use crate::{
+    io_uring::{EventType, Uring},
+    request::Request,
+};
 use anyhow::Context as _;
 use std::{
+    fs, io,
     net::{TcpListener, TcpStream},
     os::unix::prelude::*,
+    path::Path,
 };
 
 const QUEUE_DEPTH: u32 = 256;
 const READ_SIZE: usize = 8096;
-
-const NOT_FOUND: &str = "\
-    HTTP/1.0 404 Not Found\r\n\
-    Content-type: text/html\r\n\
-    \r\n\
-    <html>\
-    <head>\
-    <title>404 Not Found</title>\
-    </head>\
-    <body>\
-    <h1>404 Not Found</h1>\
-    </body>\
-    </html>\
-";
-
-const BAD_REQUEST: &str = "\
-    HTTP/1.0 400 Bad Request\r\n\
-    Content-type: text/html\r\n\
-    \r\n\
-    <html>\
-    <head>\
-    <title>400 Bad Request</title>\
-    </head>\
-    <body>\
-    <h1>400 Bad Request</h1>\
-    </body>\
-    </html>\
-";
 
 fn main() -> anyhow::Result<()> {
     pretty_env_logger::try_init()?;
@@ -78,19 +55,12 @@ fn main() -> anyhow::Result<()> {
                     .ok_or_else(|| anyhow::anyhow!("unimplemented: continue read request"))?;
                 log::info!("{} {}", request.method, request.path);
 
-                match request.method {
-                    "GET" => {
-                        // TODO: handle HTTP methods
-                        uring
-                            .next_sqe()?
-                            .write(user_data.client_socket.unwrap(), NOT_FOUND.into())?;
-                    }
-                    _ => {
-                        uring
-                            .next_sqe()?
-                            .write(user_data.client_socket.unwrap(), BAD_REQUEST.into())?;
-                    }
-                }
+                let response_msg = handle_request(request)
+                    .unwrap_or_else(|_err| make_error_response_msg("500 Internal Server Error"));
+
+                uring
+                    .next_sqe()?
+                    .write(user_data.client_socket.unwrap(), response_msg)?;
             }
 
             EventType::Write => {
@@ -101,4 +71,81 @@ fn main() -> anyhow::Result<()> {
 
         uring.submit_sqes()?;
     }
+}
+
+fn handle_request(request: Request<'_>) -> anyhow::Result<Vec<u8>> {
+    match request.method {
+        "GET" => {
+            // FIXME: asyncify
+
+            let is_dir = request.path.ends_with('/');
+            let mut path = Path::new("public").join(&request.path[1..]);
+            if is_dir {
+                path.push("index.html");
+            }
+
+            let file = match fs::OpenOptions::new().read(true).open(&path) {
+                Ok(f) => f,
+                Err(err) if err.kind() == io::ErrorKind::NotFound => {
+                    return Ok(make_error_response_msg("404 Not Found"));
+                }
+                Err(err) => anyhow::bail!(err),
+            };
+
+            let metadata = file.metadata()?;
+            let mut content = io::BufReader::new(file);
+
+            let content_type = match path.extension().and_then(|ext| ext.to_str()) {
+                Some("jpg") | Some("jpeg") => "image/jpg",
+                Some("png") => "image/png",
+                Some("gif") => "image/gif",
+                Some("html") | Some("htm") => "text/html",
+                Some("js") => "application/javascript",
+                Some("css") => "text/css",
+                Some("txt") => "text/plain",
+                Some("json") => "application/json",
+                _ => "text/plain",
+            };
+            let content_length = metadata.len();
+
+            let mut response_msg = vec![0u8; 0];
+            use std::io::Write as _;
+            write!(
+                &mut response_msg,
+                "\
+                    HTTP/1.1 200 OK\r\n\
+                    Content-Type: {content_type}\r\n\
+                    Content-Length: {content_length}\r\n\
+                    \r\n\
+                ",
+                content_type = content_type,
+                content_length = content_length,
+            )?;
+
+            io::copy(&mut content, &mut response_msg)?;
+
+            Ok(response_msg)
+        }
+        _ => Ok(make_error_response_msg("400 Bad Request")),
+    }
+}
+
+fn make_error_response_msg(status: &str) -> Vec<u8> {
+    format!(
+        "\
+            HTTP/1.1 {status}\r\n\
+            Content-type: text/html\r\n\
+            \r\n\
+            <html>\
+            <head>\
+            <title>{status}</title>\
+            </head>\
+            <body>\
+            <h1>{status}</h1>\
+            </body>\
+            </html>\
+        ",
+        status = status
+    )
+    .into()
 }
